489699cc2ec223da19ea4e4979b4c083a37d8fc6
Edwin Lecona <ing.rob.edwin.lecona@outlook.com>
2019-07-17 16:16:58 -0500

    Imagenes agregadas

--- photos/models.py
+++ photos/models.py
@@ -16,7 +16,7 @@ LICENSES = (
 
 class Photo(models.Model):
     name = models.CharField(max_length=150)  # Nombre con un maximo de 150 caracteres
-    url = models.URLField()  # Fuente de la foto
+    url = models.URLField(blank=True, null=True, default="")  # Fuente de la foto
     description = models.TextField(blank=True, null=True, default="")  # Opcion de dejan en blanco el campo
     created_at = models.DateTimeField(auto_now_add=True)  # Guarda el valor del instante de tiempo en que se guarda el objeto
     modified_at = models.DateTimeField(auto_now=True)  # Cada vez que se guarde la foto se actualiza
--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Photo
 
 # Create your views here.
 #def home(request):
     #return HttpResponse('')
 def post_list(request):
-    return render(request, 'photos/post_list.html', {})
+    posts = Photo.objects.order_by('created_at')
+    return render(request, 'photos/post_list.html', {'posts' : posts})
 
